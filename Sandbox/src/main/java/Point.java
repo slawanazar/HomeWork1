5e476c4128d670d17f37fc7579028ca5796729f2
slawanazar <slawanazar@yandex.ru>
2022-10-18 17:01:09 +0300

    ДЗ 2(Создание класса Point)

---
+++ Sandbox/src/main/java/Point.java
@@ -0,0 +1,19 @@
+    public class Point {
+
+        public static void main(String[] agrs) {
+            Point p1 = new Point(6, 5);
+            Point p2 = new Point(4, 2);
+
+            System.out.println("Расстояние между точкой p1 и точкой p2 = " + p1.distance(p2));
+        }
+        public double x;
+        public double y;
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double distance(Point p1) {
+            return Math.sqrt((this.x - p1.x) * (this.x - p1.x) + (this.y - p1.y) * (this.y - p1.y));
+        }
+    }
