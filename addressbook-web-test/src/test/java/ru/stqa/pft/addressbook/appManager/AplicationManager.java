4f3b702eaf83f6303d16f89867d20f80488c6bf3
slawanazar <slawanazar@yandex.ru>
2022-12-20 17:24:28 +0300

    добавлена возможность удалённого запуска браузера

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/AplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appManager/AplicationManager.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -32,12 +35,18 @@ public class AplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/local.properties", target))));
         dbhelper = new DbHelper();
-        if (browser.equals(Browser.FIREFOX)) {
-            driver = new FirefoxDriver();
-        } else if (browser.equals(Browser.CHROME)) {
-            driver = new ChromeDriver();
-        } else if (browser.equals(Browser.IE)) {
-            driver = new InternetExplorerDriver();
+        if ("".equals(properties.getProperty("senenium.server"))) {
+            if (browser.equals(Browser.FIREFOX)) {
+                driver = new FirefoxDriver();
+            } else if (browser.equals(Browser.CHROME)) {
+                driver = new ChromeDriver();
+            } else if (browser.equals(Browser.IE)) {
+                driver = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(String.valueOf(browser));
+            driver = new RemoteWebDriver(new URL(properties.getProperty("senenium.server")), capabilities);
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
